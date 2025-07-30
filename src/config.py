77db0d849fa2414e0c3d4366c949a056ce045ed0
John Simonsen <jsimo60@wgu.edu>
2025-07-30 09:59:11 -0600

    created scraper.py and config.py scraper.py can batch load urls from urls_to_scrape.txt

---
+++ src/config.py
@@ -0,0 +1,6 @@
+#Text file containing a list of base urls to scrape, one per line
+URLS_FILE = "urls_to_scrape.txt"
+#directory where all scraped text files will be saved
+OUTPUT_DIR = "scrapped_data"
+#a delay between requests to be polite to the server
+REQUEST_DELAY = 1 #in seconds
---
+++ src/scraper/scraper.py
@@ -0,0 +1,29 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+import time
+import re
+import logging
+from ..config import URLS_FILE, OUTPUT_DIR, REQUEST_DELAY
+
+# --- Helper Functions ---
+
+def load_base_url(filepath):
+	"""
+	Loads a list of base URLs from a text file.
+
+	ARGS:
+		filepath: str, location of text file containing the list of URLS
+	RETURNS:
+		urls: list, list of base URL strings
+	"""
+	if not os.path.exists(filepath):
+		logging.error(f"Error: URL file not found at '{filepath}'")
+		return []
+	with open(filepath, 'r') as f:
+		urls = [line.strip() for line in f.readlines()]
+	return urls
+
+if __name__ == '__main__':
+	print(load_base_url(URLS_FILE))
