a149bc34c8ea1b1edae034432fcdfbab575f543f
John Simonsen <jsimo60@wgu.edu>
2025-07-31 16:07:46 -0600

    fixed a typo in the embedding model name in vectorizer.config

--- src/vectorizer/config.py
+++ src/vectorizer/config.py
@@ -1,6 +1,6 @@
 #a good, lightweight Sentence Transformer model for general purpose
 # we'll use to create embeddings
-EMBEDDING_MODEL_NAME = "all_MiniLM-L6-v2"
+EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
 #directory for the chroma db
 PERSIST_DIRECTORY = "chroma_db"
 #name of the collection within chromadb
