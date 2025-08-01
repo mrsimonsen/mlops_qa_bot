9466a91bb038f406b61971650267643b7be5ca30
John Simonsen <jsimo60@wgu.edu>
2025-08-01 14:20:22 -0600

    created pyproject, updated src/config and removed pytest.ini

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 from pathlib import Path
-import os
+import logging
 
 ROOT_DIR = Path(__file__).parent.parent
 DATA_DIR = ROOT_DIR / "data"
 SCRAPED_DATA_DIR = DATA_DIR / "scrapped_data"
 PROCESSED_DATA_DIR = DATA_DIR / "processed_data"
 DB_DIR = DATA_DIR / "chroma_db"
+URLS_FILE = DATA_DIR / "urls_to_scrape.txt"
+LOGGING_LEVEL = logging.INFO
