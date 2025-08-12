5bb41b6408697ea490b91782b2969ce752f52a47
John Simonsen <jsimo60@wgu.edu>
2025-08-12 11:28:56 -0600

    added unset docker env

--- scripts/stop_dev_environment.sh
+++ scripts/stop_dev_environment.sh
@@ -5,6 +5,8 @@ kubectl delete deployment qa-bot-app
 kubectl delete service qa-bot-service
 kubectl delete deployment ollama
 kubectl delete service ollama-service
+echo "---Resetting Docker environment---"
+eval $(minikube -p minikube docker-env -u)
 echo "--- Stopping Kubernetes cluster ---"
 minikube stop
 # minikube delete - fresh start in case of corruption
